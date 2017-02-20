21cd21273dce94c7a94e532571d13e4dbd2ce867
ww420 <contact@juliendargelos.com>
2017-02-20 13:50:20 +0100

    Adds Google Analytics

--- javascripts/main.js
+++ javascripts/main.js
@@ -636,9 +636,27 @@ var main = {
 			this.audio.pause();
 		}
 	},
+	analytics: {
+		init: function() {
+			(function(i,s,o,g,r,a,m) {
+				i.GoogleAnalyticsObject = r;
+				i[r] = i[r] || function() {(i[r].q = i[r].q || []).push(arguments);};
+				i[r].l = 1*new Date();
+				a = s.createElement(o);
+				m = s.getElementsByTagName(o)[0];
+				a.async = 1;
+				a.src = g;
+				m.parentNode.insertBefore(a, m);
+			})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+			ga('create', 'UA-88797131-3', 'auto');
+			ga('send', 'pageview');
+		}
+	},
 	init: function() {
 		var self = this;
 		this.update(true);
+		this.analytics.init();
 		setInterval(function() {
 			self.update();
 		}, 1000);
